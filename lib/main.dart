78b0ad8c720dcabb2dbaa1f059e4077b4aa98de3
progLearn26355800 <112737082+progLearn26355800@users.noreply.github.com>
2023-10-09 09:33:12 +0000

    タイトル変更

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'おうちIoT',
       theme: ThemeData(
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         primarySwatch: Colors.blue,
